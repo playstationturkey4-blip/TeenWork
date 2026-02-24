0176fede2fd6d120ee5c3fa2306788be5f871012
playstationturkey4-blip <playstationturkey4@gmail.com>
2026-02-24 14:52:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// Конфигурация Supabase
-const SUPABASE_URL = 'https://edltxsziwwvbdnpblxzc.supabase.co'; //
-const SUPABASE_KEY = 'sb_publishable_I8nurQtNC1F-NcYar0bxxQ_vrYHSEXa'; //
-
-// Инициализация клиента
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Переименовываем supabase в mySupabase, чтобы не было конфликтов
+const mySupabase = window.supabase.createClient(
+  'https://edltxsziwwvbdnpblxzc.supabase.co', 
+  'sb_publishable_I8nurQtNC1F-NcYar0bxxQ_vrYHSEXa'
+);
