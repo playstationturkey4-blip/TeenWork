a10caaddd1a2a42f03b5c2be02695157e5dd2783
playstationturkey4-blip <playstationturkey4@gmail.com>
2026-03-02 17:54:26 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Внимательно: здесь теперь 'edit' вместо 'edlt'!
-const SUPABASE_URL = 'https://editxsziwwvbdnpblxzc.supabase.co';
-
-// Твой ключ из раздела API
-const SUPABASE_KEY = 'sb_publishable_I8nurQtNC1F-NcYar0bxxQ_vrYHSEXa'; 
-
-// Инициализируем клиент
-const mySupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
